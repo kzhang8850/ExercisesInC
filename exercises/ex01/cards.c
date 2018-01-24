f0ecfe9a765675582308547e86300ba5e291c76e
Kevin Zhang <kzhang@ivani.com>
2018-01-23 22:23:30 -0500

    more or less finished it except for optimization, will come back

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,38 +1,92 @@
+/*
+Exercise code meant to take in a card name from a standard deck
+and then change a count value accordingly to a basic set of rules
+
+Borrowed from page 37 of Head First C by Griffith and Griffith
+Edited by Kevin Zhang
+Spring 2018 Software Systems
+Olin College of Engineering
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+gets the inputted card name and puts it in card_name,
+truncates just to two chars
+
+prompt: display question to ask for input
+card_name: variable that stores the input
+*/
+void get_card_name(char *prompt, char *card_name){
+    puts(prompt);
+    scanf("%2s", card_name);
+
+}
+
+/*
+uses the input card name to derive a value, based on cases
+
+card_name: the input card name from get_card_name()
+val: the output value obtained from the card name and the below
+set of case rules
+*/
+void get_val(char *card_name, int *val){
+    switch(card_name[0]){
+      case 'K':
+      case 'Q':
+      case 'J':
+        *val = 10;
+        break;
+      case 'A':
+        *val = 11;
+        break;
+      case 'X':
+        *val = -1;
+        break;
+      default:
+      {
+        *val = atoi(card_name);
+        if ((*val < 1) || (*val > 10)){
+          puts("I don't understand that value!");
+          *val = -1;
+        }
+      }
+    }
+}
+
+/*
+uses val to change the count variable based on what range
+val falls into on the number line
+prints count to show its current value
+
+val: the latest val obtained from get_val()
+count: the count variable to be updated by val according
+to val's value
+*/
+void change_count(int *val, int *count){
+    if ((*val > 2) && (*val < 7)){
+      *count = *count + 1;
+    }
+    else if (*val == 10){
+      *count = *count - 1;
+    }
+    printf("Current count: %i\n", *count);
+}
+
+/*the main function, which puts the three functions above together*/
 int main() {
     char card_name[3];
     int count = 0;
+    int val = 0;
     while(card_name[0]!= 'X'){
-      puts("Enter the card_name: ");
-      scanf("%2s", card_name);
-      int val = 0;
-      switch(card_name[0]){
-        case 'K':
-        case 'Q':
-        case 'J':
-          val = 10;
-          break;
-        case 'A':
-          val = 11;
-          break;
-        case 'X':
-          continue;
-        default:
-          val = atoi(card_name);
-          if ((val < 1) || (val > 10)){
-            puts("I don't understand that value!");
-            continue;
-          }
+      get_card_name("Enter the card_name: ", card_name);
+      get_val(card_name, &val);
+      if (val == -1){ /*small check for invalid or terminating signals*/
+        continue;
       }
-      if ((val > 2) && (val < 7)){
-        count++;
-      }
-      else if (val == 10){
-        count--;
-      }
-      printf("Current count: %i\n", count);
+      change_count(&val, &count);
     }
     return 0;
 }
--- homeworks/homework01.md
+++ homeworks/homework01.md
@@ -8,15 +8,29 @@
 
 1) Give an example of a feature common in interpreted languages that is rare in compiled languages.
 
+A feature common in interpreted languages but rare in compiled languages is dynamic types. This generally means that interpreted languages declare types at runtime, whereas compiled languages declare them at compilation.
+
 2) Name two advantages of static typing over dynamic typing.
+
+One advantage is that errors can be found more quickly because checks on the correct types for arguments and returns happen at compile time, which also means that errors can be found on portions that have never been run. Another advantage is that static typing saves space, as names of variables exists only during compile time, giving runtime more room.
 
 3) Give an example of a static semantic error.
 
+An example is an error where a function is called without the right number of arguments of the wrong type of arguments.
+
 4) What are two reasons you might want to turn off code optimization?
 
+One reason is that it makes debugging easier, as increasing optimization levels can bring about or hide subtle bugs, which might change the behavior of the program and cause confusion. Another reason is if you are developing new code, as leaving off as many possible confounding variables when making something you are not familiar with is a good idea. As a small tidbit, optimization seems to also increase compile time with increasingly higher tier optimization levels, so especially for rapid prototyping to make something work, faster compile times and more iterations on code would be favored over "clean" code that might not work yet.
+
 5) When you run `gcc` with `-S`, why might the results look different on different computers?
+
+gcc is usually configured to generate machine code for the machine you are running. So machines with different architectures from the conventional x86 assembly language type that processors like Intel, AMD, etc. use might see a different result.
 
 6) If you spell a variable name wrong, or if you spell a function name wrong, the error messages
 you get might look very different.  Why?
 
+This is because syntax errors and function miscalls are checked in different parts of the compilation process and thus different "entities" of the C language. Spelling a variable name wrong is a syntax error and thus is checked by the compiler. Spelling a function name wrong is considered an undefined function, and thus is checked by the linker. It would then be easy to say that different entities that check different things in the various of the compilation process would produce different error messages.
+
 7) What is a segmentation fault?
+
+A segmentation fault is trying to read or write an incorrect location in memory.
